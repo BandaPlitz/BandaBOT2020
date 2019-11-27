9df326b2002bb3376f951a3c41f48acd0cbdf590
adars <adars@DESKTOP-A2AFUN8>
2019-11-27 20:03:48 +0200

    created color reset run

--- src/user/runs/ColourReset.java
+++ src/user/runs/ColourReset.java
@@ -1,8 +1,11 @@
 package user.runs;
 
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
 import robot.RobotMap;
 import robot.runs.RobotRun;
-import robot.runs.RunHandler;
+import robot.utils.Wait;
+import robot.utils.WaitCondition;
 import user.utils.LineFollow;
 
 public class ColourReset extends RobotRun {
@@ -14,11 +17,27 @@ public class ColourReset extends RobotRun {
 
 	@Override
 	public void runInstructions() {
-		//while (RunHandler.isRunning()) {
-			//System.out.println(RobotMap.getSensor("lcolours").read());
-		LineFollow.follow("lcolours", "right", 0.7, 0.5);
-			
+		LCD.drawString("Place Lcolor sensor \n on black", 2, 0);
 		
+		WaitCondition enterPressed = new WaitCondition() {
+			
+			@Override
+			public boolean evaluate() {
+				return Button.getButtons() == Button.ID_ENTER;
+			}
+		};
+		
+		Wait.waitFor(enterPressed);
+		
+		LineFollow.black = RobotMap.getSensor("lcolours").read();
+		
+		LCD.drawString("Place Lcolor sensor \n on white", 2, 0);
+		
+		Wait.waitFor(enterPressed);
+		
+		LineFollow.white = RobotMap.getSensor("lcolours").read();
+		
+		System.out.println("white: " + LineFollow.white + "\n black: " + LineFollow.black);
 	}
 
 	
