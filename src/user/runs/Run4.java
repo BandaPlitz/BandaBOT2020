c4918c42360c8c51e18e0419470266068bd9013e
adars <adars@DESKTOP-A2AFUN8>
2020-02-06 14:45:48 +0200

    rtpoejrt 9/12

--- src/user/runs/Run4.java
+++ src/user/runs/Run4.java
@@ -32,9 +32,9 @@ Condition enterPressed = new Condition() {
 		GyroFollow.followDegrees(Convert.cmToDegrees(80), 0, 0.9, 0.6, 0.2, false);
 	    Wait.waitForSeconds(0.01);
 		
-		RobotMap.getMotor("rarm").rotateDegrees(0.4, 80, false);
+		RobotMap.getMotor("rarm").rotateDegrees(0.3, 60, false);
 		RobotMap.getMotor("rarm").rotateDegrees(-0.5, 50, true);
-		GyroFollow.followDegrees(Convert.cmToDegrees(15), 0, 0.2, -0.2, false);
+		/*GyroFollow.followDegrees(Convert.cmToDegrees(15), 0, 0.2, -0.2, false);
 		RobotMap.getChassis().tankDriveDegrees(0.4, -0.4, 130, true);
 		GyroFollow.followDegrees(Convert.cmToDegrees(69), -45, 0.4, 0.6, true);
 		RobotMap.getChassis().tankDriveDegrees(0.25, -0.25, 130, false);
@@ -42,7 +42,7 @@ Condition enterPressed = new Condition() {
 		GyroFollow.followDegrees(Convert.cmToDegrees(80), -15, 0.9, -0.9, true);
 		RobotMap.getChassis().brake();
 		Wait.waitFor(enterPressed);
-		
+		*/
 		
 	}
 
